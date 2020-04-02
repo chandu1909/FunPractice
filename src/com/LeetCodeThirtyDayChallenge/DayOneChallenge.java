92d0b2dc6a1a2df19bc0f3ebebd5ef81d44df2f9
chandra sekhar <chandra.polavarapu516@topper.wku.edu>
2020-04-01 21:38:03 -0700

    feactored brute force method to more efficient one

--- src/com/LeetCodeThirtyDayChallenge/DayOneChallenge.java
+++ src/com/LeetCodeThirtyDayChallenge/DayOneChallenge.java
@@ -1,38 +1,20 @@
 package com.LeetCodeThirtyDayChallenge;
-
 import java.util.HashMap;
 import java.util.Map;
-
 public class DayOneChallenge {
-
-
     public int singleNumber(int nums[]) {
-
         HashMap<Integer, Integer> numberMap = new HashMap();
-        int counter = 0;
-        try {
-            for (int i = 0; i < nums.length; i++) {
-                //we will store element and the counter in a hashmap
-                if (!numberMap.containsKey(nums[i])) {
-                    numberMap.put(nums[i], 1);
-                } else {
-                    counter = numberMap.get(nums[i]);
-                    counter++;
-                    numberMap.put(nums[i], counter);
-                }
-            }
-            for (Map.Entry<Integer, Integer> entry : numberMap.entrySet()) {
-                if (entry.getValue().equals(1)) {
-                    return entry.getKey();
-                }
-            }
-
-        } catch (Exception e) {
-            System.out.println(e);
+        for (Integer i : nums) {
+            numberMap.put(i, numberMap.getOrDefault(i, 0) + 1);
         }
+        for (Map.Entry<Integer, Integer> entry : numberMap.entrySet()) {
+            if (entry.getValue().equals(1)) {
+                return entry.getKey();
+            }
+        }return 0;
 
-
-        return 0;
+        //we can alo write the entire 10 lines of code above in one line below.
+        //return Arrays.stream(nums).reduce(0, (acc, x) -> acc ^ x);
     }
 }
 
