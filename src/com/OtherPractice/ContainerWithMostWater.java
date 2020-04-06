a84574c37b484ae4b2e8e7964f9810adcdb2230d
chandra sekhar <chandra.polavarapu516@topper.wku.edu>
2020-04-05 19:14:12 -0700

    water container problem solvd

---
+++ src/com/OtherPractice/ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+package com.OtherPractice;
+
+public class ContainerWithMostWater {
+
+    public int process(int height[]) {
+
+        if (height.length == 0 || height.length < 2) {
+            return 0; //cz we cannot store water if there is no bowl
+        }
+
+        int max = 0;
+        int leftPointer = 0;
+        int rightPointer = height.length - 1;
+
+        while (leftPointer < rightPointer) {
+            max = Math.max(max, (rightPointer - leftPointer) * Math.min(height[leftPointer], height[rightPointer]));
+            if (height[leftPointer] < height[rightPointer]) {
+                leftPointer++; //moving to the right
+            } else {
+                rightPointer--; //moving to the left
+            }
+        }
+        System.out.print(max);
+        return max;
+    }
+}
---
+++ src/com/OtherPractice/Main.java
@@ -0,0 +1,9 @@
+package com.OtherPractice;
+
+public class Main {
+    public static void main(String args[]){
+        ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+        int height[]={1,8,6,2,5,4,8,3,7};
+        containerWithMostWater.process(height);
+    }
+}
