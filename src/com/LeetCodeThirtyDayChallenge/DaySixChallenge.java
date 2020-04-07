529d2a90bb61e44d1d9f75847f1f0249c1c927f2
chandra sekhar <chandra.polavarapu516@topper.wku.edu>
2020-04-06 19:32:48 -0700

    a proper solution for DaySixChallenge

--- src/com/LeetCodeThirtyDayChallenge/DaySixChallenge.java
+++ src/com/LeetCodeThirtyDayChallenge/DaySixChallenge.java
@@ -1,56 +1,26 @@
 package com.LeetCodeThirtyDayChallenge;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
- * @Description: Returns a list of lists
+ * @Author: Chandra gsekhar Polavarapu
+ * @Description: Returns a list of lists with all angrams in the given array
  */
 
 public class DaySixChallenge {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List <List <String>> resultList = new ArrayList<>();
-
-        //making sure the number of elements are at least 2
-        if (strs.length==0){
-            return new ArrayList<>();
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
         }
+        System.out.println(ans.values());
+        return new ArrayList(ans.values());
 
-        List<String> avoidList = new ArrayList<>();
-
-        for (int i = 0; i< strs.length;i++){
-
-            if (avoidList.contains(strs[i])==false){
-                List<String> newList = new ArrayList<String>();
-                newList.add(strs[i]);
-                for (int j =i+1; j< strs.length;j++){
-                    if (strs[i].length() == strs[j].length()){
-                        boolean result = processWords(strs[i],strs[j]);
-                        if (result && avoidList.contains(strs[j])==false && i!=j){
-                            newList.add(strs[j]);
-                        }
-                    }
-                }
-                resultList.add(newList);
-                avoidList.addAll(newList);
-            }
-        }
-        System.out.println(resultList);
-        return resultList;
-    }
-
-    private boolean processWords(String str1, String str2) {
-        char[] ca = str1.toCharArray();
-        Arrays.sort(ca);
-        char[] ca2 = str2.toCharArray();
-        Arrays.sort(ca2);
-
-        if (Arrays.equals(ca,ca2)){
-            return true;
-        }
-        return false;
     }
 }
