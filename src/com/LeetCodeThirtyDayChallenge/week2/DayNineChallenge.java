bdb3836d09629b980a1fb50b9e0929982c17f0e7
chandra sekhar <chandra.polavarapu516@topper.wku.edu>
2020-04-09 19:34:03 -0700

    Day9 challenge finished

--- src/com/LeetCodeThirtyDayChallenge/week2/DayNineChallenge.java
+++ src/com/LeetCodeThirtyDayChallenge/week2/DayNineChallenge.java
@@ -1,9 +1,34 @@
 package com.LeetCodeThirtyDayChallenge.week2;
 
+/**
+ * @Author: Chandra sekhar Poalvarapu
+ * Description: Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+ * Input: S = "ab#c", T = "ad#c"
+ * Output: true
+ * Explanation: Both S and T become "ac".
+ */
 public class DayNineChallenge {
     public boolean backspaceCompare(String S, String T) {
 
-        System.out.println(S+T);
+        String resultS = process(S);
+        String resultT = process(T);
+
+        if (resultS.equals(resultT)) return true;
+
         return false;
+    }
+
+    public String process(String S) {
+        StringBuilder resultS = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            char character = S.charAt(i);
+            if (Character.isAlphabetic(character)) { //checking if the character is alphabet
+                resultS.append(S.charAt(i));
+            } else if (character == '#' && resultS.length() != 0) {
+                resultS.deleteCharAt(resultS.length() - 1); //if the current character is # we need to backspace the previous character
+            }
+        }
+
+        return resultS.toString();
     }
 }
